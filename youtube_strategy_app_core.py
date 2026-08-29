e7fa5f0d8dcb3c59979afe05f60b45b25fbbb10b
derektshaffer <49662101+derektshaffer@users.noreply.github.com>
2026-08-28 20:50:07 -0700

    Lock sidebar open in youtube_strategy_app_core.py

--- youtube_strategy_app_core.py
+++ youtube_strategy_app_core.py
@@ -57,7 +57,7 @@ st.set_page_config(
     page_title="YouTube Trading Strategy Lab",
     page_icon="🎬",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="locked",
 )
 require_app_access(st)
 
